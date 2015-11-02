0c9dc9376ef7c159ab09ac815d33762a5400202f
tristan-k <mail@tristank.de>
2015-11-02 15:02:19 +0100

    added dockdoger

---
+++ Casks/dockdodger.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'dockdodger' do
+  version '0.1'
+  sha256 '0dd4c4956d2fe0b223cd84cdeca8f82ffc51bf90ebdd0fb599c61459dfba2eeb'
+
+  url 'http://www.macupdate.com/download/24427/Dock%20Dodger.zip'
+  name 'Dock Dodger'
+  homepage 'http://foggynoggin.com/'
+  license :gratis
+
+    app 'Dock Dodger.app'
+end
