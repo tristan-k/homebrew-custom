4a9773af93b132bbbbfd0cd0baac8c4aefcbbe68
tristan-k <mail@tristank.de>
2015-11-02 14:12:35 +0100

    proper download url

--- Casks/cryptomator.rb
+++ Casks/cryptomator.rb
@@ -2,7 +2,8 @@ cask :v1 => 'cryptomator' do
   version '0.10.0'
   sha256 'a696d01cae49e15a95dda928549cd2d208dcf3c374dea290dfefefed00046366'
 
-  url 'https://bintray.com/artifact/download/cryptomator/cryptomator/Cryptomator-0.10.0.dmg:'
+  # bintray.com is the official download host per the vendor homepage
+  url 'https://dl.bintray.com/cryptomator/cryptomator/Cryptomator-#{version}.dmg:'
   name 'Cryptomator'
   homepage 'https://cryptomator.org'
   license :mit
