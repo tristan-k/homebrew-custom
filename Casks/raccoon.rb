80852ff44d2f87bb63886b7213d70ab1c88af1e5
tristan-k <mail@tristank.de>
2015-11-02 15:43:42 +0100

    added raccoon & sound siphon

---
+++ Casks/raccoon.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'raccoon' do
+  version '3.5'
+  sha256 '89e1eeba1d82cbd5bdb4f42f0aa8e0b4e009d9f8bf5fbd801f6f62ef77b5a096'
+
+  url 'http://www.onyxbits.de/sites/default/files/download/25/raccoon-3.5.jar'
+  name 'Raccoon'
+  homepage 'http://www.onyxbits.de/raccoon'
+  license :gratis
+
+#  app 'Raccoon.app'
+end
---
+++ Casks/soundsiphon.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'soundsiphon' do
+  version '2.0.1'
+  sha256 'fe74403d9aa7ba95f56070c376f30d4aa4eea814fecc9319767215605f6d085c'
+
+  url 'http://staticz.com/download/1837/'
+  name 'Sound Siphon'
+  homepage 'hhttp://staticz.com/'
+  license :commercial
+
+  app 'Sound Siphon.app'
+end
