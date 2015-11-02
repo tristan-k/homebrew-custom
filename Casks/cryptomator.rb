90b2647aacb28665a800accc4d8acbdfed4c58a6
tristan-k <mail@tristank.de>
2015-11-02 14:29:28 +0100

    fix url again

--- Casks/cryptomator.rb
+++ Casks/cryptomator.rb
@@ -3,7 +3,7 @@ cask :v1 => 'cryptomator' do
   sha256 'a696d01cae49e15a95dda928549cd2d208dcf3c374dea290dfefefed00046366'
 
   # bintray.com is the official download host per the vendor homepage
-  url 'https://dl.bintray.com/cryptomator/cryptomator/Cryptomator-#{version}.dmg'
+  url "https://dl.bintray.com/cryptomator/cryptomator/Cryptomator-#{version}.dmg"
   name 'Cryptomator'
   homepage 'https://cryptomator.org'
   license :mit
