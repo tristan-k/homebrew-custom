b77173486a0463906fb14ea3d7d6adcefd56354c
tristan-k <mail@tristank.de>
2015-11-02 14:49:21 +0100

    fix typo in version

--- Casks/appsassist.rb
+++ Casks/appsassist.rb
@@ -1,5 +1,5 @@
 cask :v1 => 'appsassist' do
-  version '1.0.1
+  version '1.0.1'
   sha256 '8efdf9b21c9930beafaa2c9ae6a2315dd56eda60fbf4823a0df4da1b6dd94276'
 
   url 'http://dougscripts.com/itunes/scrx/appsassistv1.zip'
