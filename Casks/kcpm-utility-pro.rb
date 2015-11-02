2fb5e428bba1b0146f767cfc5f296c544bb0fd20
tristan-k <mail@tristank.de>
2015-11-02 15:23:05 +0100

    added kcpm-utility-pro

---
+++ Casks/kcpm-utility-pro.rb
@@ -0,0 +1,11 @@
+ cask :v1 => 'kcpm-utility-pro' do
+   version '3.3.3'
+   sha256 '5ad915d2e8ec0a36a4032409d027a9be3ac92be909569a67e34cff62efd1b783'
+
+   url 'https://www.firewolf.science/wp-content/RSS/KCPM%20Utility%20Pro%203.3.zip'
+   name 'KCPM Utility Pro'
+   homepage 'http://www.firewolf.science/2015/10/kcpm-utility-pro-v3-0-installing-kexts-repairing-permissions-rebuilding-caches-configuring-sip-and-more/'
+   license :gratis
+
+   app 'KCPM Utility Pro.app'
+ end
