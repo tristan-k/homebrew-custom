6100f1f6d21bdec2d9c21ab6aa9891378b2681f0
tristan-k <mail@tristank.de>
2015-11-02 14:41:47 +0100

    added appsassist

---
+++ Casks/appsassist.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'appsassist' do
+  version '1.0.1
+  sha256 '8efdf9b21c9930beafaa2c9ae6a2315dd56eda60fbf4823a0df4da1b6dd94276'
+
+  url 'http://dougscripts.com/itunes/scrx/appsassistv1.zip'
+  name 'Apps Assist'
+  homepage 'http://dougscripts.com/itunes/itinfo/appsassistapp.php'
+  license :freemium
+
+    app 'Apps Assist.app'
+end
