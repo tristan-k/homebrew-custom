432b469e5ca0f32d4400e158ba9e947bf3dd1c92
tristan-k <mail@tristank.de>
2015-11-27 22:39:40 +0100

    added qlMoviePreview

---
+++ Casks/qlmoviepreview.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'qlmoviepreview' do
+  version '1.0'
+  sha256 '1e97195f34a06357d1b9deda1a36711a2abde8b4f71379ac16f1fe2c3ce202ab'
+
+  url 'http://repo.whine.fr/qlmoviepreview.qlgenerator-10.9.zip'
+  name 'qlMoviePreview'
+  homepage 'https://github.com/Nyx0uf/qlMoviePreview'
+  license :oss
+
+  qlplugin 'qlMoviePreview.qlgenerator'
+end
