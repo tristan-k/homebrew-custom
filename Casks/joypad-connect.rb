21f36a94ae64d28e2aa6220841821f6759e0101b
tristan-k <mail@tristank.de>
2015-12-03 15:12:31 +0100

    added joypad-connect

---
+++ Casks/joypad-connect.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'joypad-connect' do
+  version '1.5.2'
+  sha256 '068a9d8b800f5f60e48576f6d21a12c8fc05047b2c4e0a145c320cf11190d842'
+
+  url 'http://getjoypad.com/joypadconnect/JoypadConnect-1.5.2.dmg'
+  name 'Dock Dodger'
+  homepage 'http://getjoypad.com/'
+  license :gratis
+
+    app 'Joypad Connect.app'
+end
