bc33684144b904760fcdc386759d6f1001733925
tristan-k <mail@tristank.de>
2015-12-10 13:32:41 +0100

    fixed typo

--- Casks/owndrop.rb
+++ Casks/owndrop.rb
@@ -2,7 +2,7 @@ cask :v1 => 'owndrop' do
   version '0.8'
   sha256 'eb7d8c5bd5d3e322651e5906f8a9b945e93e706a301e085b7f745f0c1eced8db'
 
-  url 'https://github.com/Leandros/ownDrop/releases/download/0.8.0/ownDrop-0.8.0.zip
+  url 'https://github.com/Leandros/ownDrop/releases/download/0.8.0/ownDrop-0.8.0.zip'
   name 'ownDrop'
   homepage 'https://github.com/Leandros/ownDrop'
   license :gpl
