a9d897481f7f0ea58925215ca7a26feec2a99fb9
tristan-k <mail@tristank.de>
2015-12-13 13:17:28 +0100

    added colordial

---
+++ Casks/colordial.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'colordial' do
+  version '1.6'
+  sha256 '2fe1c26bb9c74c1278bfe7f2df7626da405980e4dc258873a3c1516127d294eb'
+
+  url 'https://github.com/NorgannasAddOns/ColorDial/releases/download/1.6.32/ColorDial-1.6.zip'
+  name 'ColorDial'
+  homepage 'https://github.com/NorgannasAddOns/ColorDial'
+  license :oss
+
+    app 'ColorDial.app'
+end
