74ccf4c25cbfac3a92757644135ec962568cf9d3
tristan-k <mail@tristank.de>
2016-01-19 23:55:41 +0100

    back again

--- Casks/qtox-osx.rb
+++ Casks/qtox-osx.rb
@@ -3,7 +3,7 @@ cask :v1 => 'qtox-osx' do
   sha256 '0911dab7e9ea993c6f67a93dd652b83ad168aa1fd7b0ae1eee8eb353750c2cb1'
 
   url 'https://github.com/RowenStipe/qTox-OSX/releases/download/1.2.2.184/qTox-OSX-1.2.2.184.dmg'
-  name 'qtox-osx'
+  name 'qTox'
   homepage 'https://blog.tox.chat/'
   license :gpl
 
