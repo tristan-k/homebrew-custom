aac9871b5ba9084b7cc3f7e71633ea93d3e36dc4
tristank <tristank@tk-pb4540s-macosx.local>
2016-06-02 17:37:18 +0200

    bugfixes

--- Casks/colordial.rb
+++ Casks/colordial.rb
@@ -3,7 +3,7 @@ cask :v1 => 'colordial' do
   sha256 '2fe1c26bb9c74c1278bfe7f2df7626da405980e4dc258873a3c1516127d294eb'
 
   url 'https://github.com/NorgannasAddOns/ColorDial/releases/download/1.6.32/ColorDial-1.6.zip'
-  name 'ColorDial'
+  name 'colorDial'
   homepage 'https://github.com/NorgannasAddOns/ColorDial'
   license :oss
 
